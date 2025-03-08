b423819415ade1a3c3e7f3b25098ab7504a3f4ee
Anil <anilkumar7062000@gmail.com>
2025-03-08 20:39:22 +0530

    spring boot project setup

---
+++ src/main/java/com/test/securitydemo/controllers/NonSecuredController.java
@@ -0,0 +1,18 @@
+package com.test.securitydemo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class NonSecuredController {
+
+    @GetMapping("/greet")
+    public ResponseEntity<String> greet(){
+        return ResponseEntity
+                .status(200)
+                .body("Hi, This is public page");
+    }
+}
