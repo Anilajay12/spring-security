07455f55ef9344c2eb12973a0f50ebbdad84c576
Anil <anilkumar7062000@gmail.com>
2025-03-08 21:08:51 +0530

    Update changes for default username with inmemory

--- src/main/java/com/test/securitydemo/config/ApplicationSecurityConfig.java
+++ src/main/java/com/test/securitydemo/config/ApplicationSecurityConfig.java
@@ -6,7 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +27,12 @@ public class ApplicationSecurityConfig {
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetails(){
+        UserDetails admin = User.withUserDetails(new User("admin", "{noop}admin", List.of(new SimpleGrantedAuthority("ADMIN")))).build();
+        UserDetails user = User.withUserDetails(new User("user", "{noop}user", List.of(new SimpleGrantedAuthority("USER")))).build();
+        return new InMemoryUserDetailsManager(List.of(admin, user));
     }
 }
