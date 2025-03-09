e24c3c040e75427f5d653a28159af8c8c5268e70
Anil <anilkumar7062000@gmail.com>
2025-03-09 11:20:45 +0530

    updated inmemory authentication and authorization

--- src/main/java/com/test/securitydemo/config/ApplicationSecurityConfig.java
+++ src/main/java/com/test/securitydemo/config/ApplicationSecurityConfig.java
@@ -3,9 +3,15 @@ package com.test.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +25,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class ApplicationSecurityConfig {
 
     @Bean
@@ -28,6 +35,7 @@ public class ApplicationSecurityConfig {
                 .authorizeHttpRequests(request -> request.requestMatchers("/public/**").permitAll()
                         .requestMatchers( "/h2-console/**", "/actuator/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
@@ -35,12 +43,23 @@ public class ApplicationSecurityConfig {
     @Bean
     public UserDetailsService userDetails(){
         UserDetails admin = User.withUserDetails(new User("admin", passwordEncoder().encode("admin"), List.of(new SimpleGrantedAuthority("ROLE_ADMIN")))).build();
-        UserDetails user = User.withUserDetails(new User("user", "{noop}user", List.of(new SimpleGrantedAuthority("ROLE_USER")))).build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
         return new InMemoryUserDetailsManager(List.of(admin, user));
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
--- src/main/java/com/test/securitydemo/controllers/SecuredController.java
+++ src/main/java/com/test/securitydemo/controllers/SecuredController.java
@@ -2,18 +2,28 @@ package com.test.securitydemo.controllers;
 
 import com.test.securitydemo.dto.Message;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/private")
+@RequestMapping("/secured")
 public class SecuredController {
 
-    @GetMapping("/greet")
-    public ResponseEntity<Message> greet() {
+    @GetMapping("/admin/greet")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Message> greetSecuredAdmin() {
         return ResponseEntity
                 .status(200)
-                .body(new Message("Hi, This is Secured page"));
+                .body(new Message("Hi, This is Secured admin page"));
+    }
+
+    @GetMapping("/user/greet")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ResponseEntity<Message> greetSecuedUsers() {
+        return ResponseEntity
+                .status(200)
+                .body(new Message("Hi, This is Secured user page"));
     }
 }
